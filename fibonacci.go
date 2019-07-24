13aeae0544be1f8f5eaf62dc33a67f4434b3964d
arnur228 <arnrbey@mail.ru>
2019-07-25 02:20:59 +0600

    фибоначчи работает

--- fibonacci.go
+++ fibonacci.go
@@ -2,48 +2,47 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"os"
+	"strconv"
 )
 
 func main() {
 	ch := make(chan int)
-	mu := sync.Mutex{}
-	wg := sync.WaitGroup{}
+	ch1, ch2 := make(chan int), make(chan int)
+	file, _ := os.Create("fibonacci.txt")
 	a := 0
 	b := 1
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 20; i++ {
 		if a > b {
-			mu.Lock()
-			ch <- a
-			Fork(ch)
-			mu.Unlock()
+			go WriteToChannel(ch, a)
+			Fork(ch, ch1, ch2, file)
 			b += a
 		} else {
-			mu.Lock()
-			ch <- b
-			wg.Wait()
-			Fork(ch)
-			wg.Done()
-			mu.Unlock()
+			go WriteToChannel(ch, b)
+			Fork(ch, ch1, ch2, file)
 			a += b
 		}
 	}
 }
-func Fork(ch chan int) {
+func Fork(ch chan int, ch1 chan int, ch2 chan int, f *os.File) {
 	x := <-ch
-	close(ch)
-	ch1, ch2 := make(chan int), make(chan int)
-	ch1 <- x
-	ch2 <- x
+	go WriteToChannel(ch1, x)
+	go WriteToChannel(ch2, x)
 	ToConsole(ch1)
-	//	ToFile(ch2)
+	ToFile(ch2, f)
 }
 
 func ToConsole(ch chan int) {
 	x := <-ch
 	fmt.Println(x)
-	close(ch)
 }
-func ToFile(ch chan int) {
+func ToFile(ch chan int, f *os.File) {
+	x := <-ch
+	bs := []byte(strconv.Itoa(x))
+	f.Write(bs)
+	f.WriteString("\n")
+}
 
+func WriteToChannel(ch chan int, x int) {
+	ch <- x
 }
