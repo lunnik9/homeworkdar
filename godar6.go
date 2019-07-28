34a5e51e9b15d877d0d83080e418ea83ea0cc522
arnur228 <arnrbey@mail.ru>
2019-07-28 06:25:59 +0600

    проблемы на 17 строке

---
+++ godar6.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	indexString := r.URL.Path
+	indexString = strings.Replace(indexString, "/", "", -1)
+	indexInt64, _ := strconv.ParseInt(indexString, 10, 32)
+	index := int(indexInt64)
+	err := binary.Write(w, binary.LittleEndian, uint16(FibonacciCounter(index).Current))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func FibonacciCounter(index int) (f FibNumber) {
+	a := 0
+	b := 1
+	for i := 0; i < index; i++ {
+		if a > b {
+			b += a
+		} else {
+			a += b
+		}
+	}
+	if a > b {
+		f.Current = a
+		f.Prev = b
+		f.Next = a + b
+	} else {
+		f.Current = b
+		f.Prev = a
+		f.Next = a + b
+	}
+	return f
+}
+
+type FibNumber struct {
+	Current int
+	Prev    int
+	Next    int
+}
+
+func main() {
+	http.HandleFunc("/4", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
